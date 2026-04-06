b86da1b52850b738d92559e098bdb19f9e27daa8
Bruna Drielen <107259932+BruDrielen@users.noreply.github.com>
2026-04-06 09:12:34 -0300

    Delete js/js directory

--- js/js/src/linearidadeSchema.js
+++
@@ -1,5 +0,0 @@
-export function getLinearidadeRequiredIds() {
-  // Para o seu objetivo (checklist), só o desenho é obrigatório.
-  // O resto você marca como feito/não feito e o sistema lista faltantes.
-  return ["nLevels", "replicatesPerLevel"];
-}
